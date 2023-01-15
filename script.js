1afa2f8f5579d09fd8f309bb54ea2178a68dd0e1
Divanshu Soni <dragodiv18@gmail.com>
2023-01-15 22:05:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function loadData(indexVal){
     title.innerHTML = songs[indexVal].name;
     singer.innerHTML = songs[indexVal].singer;
     audio.src = "music/"+songs[indexVal].audio+".mp3";
-    if(playpause.classList.contains("fa-pause"))
+//     if(playpause.classList.contains("fa-pause"))
     audio.play();
 }
 
