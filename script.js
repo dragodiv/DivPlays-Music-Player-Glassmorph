1cfb2f70cfefc1a45cf5377d319eafefb0182249
Divanshu Soni ⍟ <dragodiv18@gmail.com>
2023-10-15 22:52:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -101,10 +101,9 @@ function loadData(indexVal){
     title.innerHTML = songs[indexVal].name;
     singer.innerHTML = songs[indexVal].singer;
     audio.src = "music/"+songs[indexVal].audio+".mp3";
-    if(playpause.classList.contains("fa-play"))
-        playpause.classList.remove("fa-play");
-        playpause.classList.add("fa-pause");
-        audio.play();
+    playpause.classList.remove("fa-play");
+    playpause.classList.add("fa-pause");
+    audio.play();
 }
 
 
